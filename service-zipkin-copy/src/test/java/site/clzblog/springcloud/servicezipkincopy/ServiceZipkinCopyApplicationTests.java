f09d7e95c061f062fdca77939f8cbdc9744b0d29
zouchengli <zouchengli@hyron.com>
2018-07-16 18:14:44 +0800

    Service tracking

---
+++ service-zipkin-copy/src/test/java/site/clzblog/springcloud/servicezipkincopy/ServiceZipkinCopyApplicationTests.java
@@ -0,0 +1,16 @@
+package site.clzblog.springcloud.servicezipkincopy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceZipkinCopyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
