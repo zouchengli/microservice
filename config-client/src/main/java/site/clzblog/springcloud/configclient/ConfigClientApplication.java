dc853d7cffa6a547c49177f772d661ccf7e475a2
zouchengli <zouchengli@hyron.com>
2018-07-16 15:52:11 +0800

    Config center cluster

--- config-client/src/main/java/site/clzblog/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/site/clzblog/springcloud/configclient/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package site.clzblog.springcloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -17,8 +19,16 @@ public class ConfigClientApplication {
     @Value("${foo}")
     String foo;
 
+    @Value("${foocopy}")
+    String foocopy;
+
     @RequestMapping("foo")
     public String foo() {
         return foo;
+    }
+
+    @RequestMapping("foocopy")
+    public String foocopy() {
+        return foocopy;
     }
 }
